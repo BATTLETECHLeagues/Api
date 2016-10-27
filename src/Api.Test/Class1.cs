2748fff3bd7b204f597c2bcaf3ce03b2790efdfe
Adrian Russell <adrian.f.russell@gmail.com>
2016-10-27 21:42:39 +1100

    add test project

---
+++ src/Api.Test/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Api.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            
+        }
+    }
+}
