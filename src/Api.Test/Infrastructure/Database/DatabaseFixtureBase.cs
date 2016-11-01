667660f4564b7075adc3640dd74a37d15c38a2a7
Adrian Russell <adrian.f.russell@gmail.com>
2016-11-01 23:14:20 +1100

    Add database base fixture.

---
+++ src/Api.Test/Infrastructure/Database/DatabaseFixtureBase.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Api.Test.Infrastructure.Database
+{
+    [TestFixture]
+    class DatabaseFixtureBase
+    {
+        
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            
+        }
+
+    }
+}
