2a73f15ceec50037ff9a398f086a39cb53d8a12a
Adrian Russell <adrian.f.russell@gmail.com>
2016-11-03 07:09:27 +1100

    Add repository interface.

---
+++ src/Api/Infrastructure/Database/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Api.Infrastructure.Database
+{
+    public interface IRepository<T> where T : class 
+    {
+    void Add(T item);
+    void Remove(T item);
+    void Update(T item);
+    T FindByID(Guid id);
+    IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+    IEnumerable<T> FindAll();
+    }
+}
